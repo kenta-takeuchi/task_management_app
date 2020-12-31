b11612a86da5b6e0c6abba4fd5ff723f2116d050
kenta-takeuchi <49678973+kenta-takeuchi@users.noreply.github.com>
2020-12-31 20:51:35 +0900

    serializerの作成

--- api/task_management/admin.py
+++ api/task_management/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Category
+from .models import Task
+from .models import Profile
+
+admin.site.register(Category)
+admin.site.register(Task)
+admin.site.register(Profile)
